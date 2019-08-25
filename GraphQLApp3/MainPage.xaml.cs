307dda753f5fec1c33008528e374e63a7982a264
Michelle Weekes <michelleweekes@wired2grafix.com>
2019-08-25 01:46:05 -0400

    Initial check-in of module GraphQLApp3

---
+++ GraphQLApp3/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraphQL.Client;
+using GraphQL.Common.Request;
+using GraphQL.Common.Response;
+using Xamarin.Forms;
+using GraphQLApp3.Models;
+
+
+namespace GraphQLApp3
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var client = new GraphQLClient("https://swapi.apis.guru/");
+
+            GraphQLRequest graphQLRequest = new GraphQLRequest
+            {
+                Query = "query{ allFilms { films { title, director, id } } }"
+            };
+
+            GraphQLResponse graphQLResponse = await client.PostAsync(graphQLRequest);
+            StarWarsItems.ItemsSource = graphQLResponse.Data.allFilms.films.ToObject<List<TitleDirector>>();
+
+        }
+    }
+}
---
+++ GraphQLApp3/Models/TitleDirector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace GraphQLApp3.Models
+{
+    using System.Collections.Generic;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class TitleDirector
+    {
+        public string title { get; set; }
+        public string director { get; set; }
+        public string id { get; set; }
+
+        public string directorId { get { return "Director: " + director ; } }
+    }
+}
+
+
+
+
+
+
